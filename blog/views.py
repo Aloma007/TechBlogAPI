b7266b97b19e8bc569357aecb3a0fbb76b2c1b85
Salami Idris Akolade <salamiidris2007@gmail.com>
2026-05-16 21:09:14 +0100

    Complete Phase 6: Implemented slug routing for Post ViewSet

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,26 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly  # Importing your new rule
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
 
+    # 1. Enforce authentication and the custom author rule
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    lookup_field = 'slug'
+    # 2. Automatically set the author to the user making the request
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include # Make sure include is imported!
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')), # This routes traffic to your blog app
+    # Added line to enable the Login/Logout button in the Browsable API
+    path('api-auth/', include('rest_framework.urls')),
 ]
